2360384f27e01ff5d16769a824b9343883c0e920
ChaithraSudheer <gchaithra9@gmail.com>
2024-02-28 21:40:11 +0530

    new line added

--- src/test/java/devOpsAssignment/SampleTest.java
+++ src/test/java/devOpsAssignment/SampleTest.java
@@ -4,6 +4,7 @@ public class SampleTest {
 
 	public static void main(String[] args) {
 		System.out.println("This is for git integration with jenkins");
+		System.out.println("New line added for testing");
 	}
 
 }
